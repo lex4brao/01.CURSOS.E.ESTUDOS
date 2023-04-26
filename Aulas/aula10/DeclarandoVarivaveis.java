36b712849d23ff9641f6f6270773c4c8ab669ed5
Lex Abrão <lex4brao@gmail.com>
2023-04-26 20:45:54 -0300

    ORGANIZANDO O REPOSITÓRIO

---
+++ Aulas/aula10/DeclarandoVarivaveis.java
@@ -0,0 +1,12 @@
+class DeclarandoVariaveis {
+  public static void main(String[] args){
+    String nome;
+    int idade;
+
+    nome = "Pandora";
+    idade = 8;
+
+    System.out.println("Nome: "+nome);
+    System.out.println("Idade: "+idade+" anos");
+  }
+}

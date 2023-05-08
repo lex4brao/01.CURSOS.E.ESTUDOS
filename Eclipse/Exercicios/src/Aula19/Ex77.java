705630bf073e648e1549d8aa5d5ca09314c54412
Lex Abrão <lex4brao@gmail.com>
2023-05-08 01:42:22 -0300

    ATUALIZAÇÃO

--- Eclipse/Exercicios/src/Aula19/Ex77.java
+++ Eclipse/Exercicios/src/Aula19/Ex77.java
@@ -3,23 +3,23 @@ package Aula19;
 import java.util.*;
 
 public class Ex77 {
-  public static void main (String[] args) {
-    Random rnd = new Random();
+	public static void main(String[] args) {
+		Random rnd = new Random();
 
-    int i=0;
-    int[] vetorA = new int[15];
-    int[] vetorB = new int[vetorA.length];
+		int i = 0;
+		int[] vetorA = new int[15];
+		int[] vetorB = new int[vetorA.length];
 
-    System.out.println("\n Imprimindo o vetor A");
-    for(i=0;i<vetorA.length;i++) {
-      vetorA[i] = rnd.nextInt(100);
-      vetorB[i] = (vetorA[i]*vetorA[i]);
-      System.out.println("vetorA["+i+"]: "+vetorA[i]);
-    } 
-    System.out.println("\n Imprimindo o vetor B");
-    for(i=0;i<vetorB.length;i++) {
-      System.out.println("vetorB["+i+"]: "+vetorB[i]);
-    } 
-  
-  }
+		System.out.println("\n Imprimindo o vetor A");
+		for (i = 0; i < vetorA.length; i++) {
+			vetorA[i] = rnd.nextInt(100);
+			vetorB[i] = (vetorA[i] * vetorA[i]);
+			System.out.println("vetorA[" + i + "]: " + vetorA[i]);
+		}
+		System.out.println("\n Imprimindo o vetor B");
+		for (i = 0; i < vetorB.length; i++) {
+			System.out.println("vetorB[" + i + "]: " + vetorB[i]);
+		}
+
+	}
 }
